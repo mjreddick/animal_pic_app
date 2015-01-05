d0d39bab027a10fd478237676524ccfdc409e31e
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-04 16:22:54 -0800

    user model authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User
-  include Mongoid::Document
+	include Mongoid::Document
 
   attr_reader :password
   # attr_accessor :password_confirmation
@@ -15,11 +15,16 @@ class User
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {in: 1..20}
   validates :pet_name, presence: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: {in: 5..50}, confirmation: true
+  validates :password, length: {in: 5..50}, confirmation: true, allow_blank: true
+  validates :password_digest, presence: true 
 
   def password=(new_password)
   	@password = new_password
-  	self.password_digest = BCrypt::Password.create(new_password)
+  	self.password_digest = BCrypt::Password.create(new_password) unless new_password.blank?
+  end
+
+  def authenticate(password_attempt)
+  	BCrypt::Password.new(password_digest).is_password?(password_attempt) && self
   end
 
 end
