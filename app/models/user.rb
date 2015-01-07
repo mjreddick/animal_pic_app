1771ba42505363ea874e8cc83b2922e043ed4187
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-06 21:46:01 -0800

    fix issue where an empty password field doesn't display a message to a the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User
 	include Mongoid::Document
 
-  attr_reader :password
-  # attr_accessor :password_confirmation
+  attr_accessor :password
 
   # field :active?, type: Boolean
   field :username, type: String
@@ -15,16 +14,23 @@ class User
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {in: 1..20}
   validates :pet_name, presence: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: {in: 5..50}, confirmation: true, allow_blank: true
-  validates :password_digest, presence: true 
+  validates :password, presence: true, length: {in: 5..50}, confirmation: true, 
+            unless: "self.password_digest.present? && self.password.blank?"
+  validates :password_digest, presence: true, if: "self.password.blank?"
 
-  def password=(new_password)
-  	@password = new_password
-  	self.password_digest = BCrypt::Password.create(new_password) unless new_password.blank?
+  before_save do
+    self.username = username.downcase
+    hash_password(self.password)
   end
 
   def authenticate(password_attempt)
-  	BCrypt::Password.new(password_digest).is_password?(password_attempt) && self
+  	BCrypt::Password.new(self.password_digest).is_password?(password_attempt) && self
   end
+
+  private
+
+    def hash_password(new_password)
+      self.password_digest = BCrypt::Password.create(new_password) unless new_password.blank?
+    end
 
 end
