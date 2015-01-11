f16cd49fd595065878870da0307440dbe3173000
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-10 17:44:17 -0800

    change uploader back to defaults

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -20,8 +20,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/pictures"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "uploads/pictures"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -76,9 +76,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    # "something.jpg" if original_filename
-    "#{model.id}" + File.extname(original_filename)
-  end
+  # def filename
+  #   # "something.jpg" if original_filename
+  #   "#{model.id}" + File.extname(original_filename)
+  # end
 
 end
