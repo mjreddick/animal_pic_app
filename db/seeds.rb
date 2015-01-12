1ffd6fdbd8eff3acde9a810fa9ec216d71fd6cd3
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-11 17:04:27 -0800

    add users and pictures to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pics = Picture.all
+pics.each do |pic|
+	pic.remove_image!
+end
 User.delete_all
-users = User.create([
-	{username: "Jimbo1", pet_name: "Jim", email: "jimbo@jimbo.com", password: "pass1"},
-	{username: "BRich", pet_name: "Micah", email: "micah@micah.com", password: "pass2"}
-	])
+Picture.delete_all
+
+usernames = ['matt', 'matthew', 'jimmy', 'james', 'marina', 'joey', 'richard',
+	'fred', 'freddy', 'tim', 'jim', 'bill', 'stan', 'stanley', 'micah', 'mike',
+	'michael', 'jeff', 'jeffery', 'mark', 'marcus', 'monica', 'ross', 'rachel'];
+
+usernames.each do |u|
+	user = User.create({username: u, email: "#{u}@example.com", password: "#{u}pass",
+		password_confirmation: "#{u}pass", pet_name: "#{u}'s pet"})
+	user.save
+	pic = user.pictures.new({title: "test"})
+	pic.image.store!(File.open("/Users/matthew/Desktop/Various Animal Pictures/smallish_cat.jpg"))
+	pic.save
+end
+
+# users = User.create([
+# 	{username: "Jimbo1", pet_name: "Jim", email: "jimbo@jimbo.com", password: "pass1"},
+# 	{username: "BRich", pet_name: "Micah", email: "micah@micah.com", password: "pass2"}
+# 	])
