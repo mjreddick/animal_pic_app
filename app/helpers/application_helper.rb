76c881471c34fa127edf1d9041583038f6d4f531
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-13 17:03:46 -0800

    add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "4 Legged Fiends"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
