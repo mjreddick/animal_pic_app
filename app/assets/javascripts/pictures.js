c8a59fa3b617b4c5f011f87f568a7f6a5ddf6060
Matthew Reddick <matthewjreddick@gmail.com>
2015-03-06 23:55:00 -0800

    add support for swipe left/right to move to prev/next picture

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,5 +1,43 @@
 $(function(){
 
+  document.addEventListener('touchstart', handleTouchStart, false);        
+  document.addEventListener('touchend', handleTouchEnd, false);
+
+  var xDown = null;                                                        
+  var yDown = null;  
+
+  function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+  }
+
+  function handleTouchEnd(evt) {
+    if ( !xDown || !yDown ) {
+      return;
+    }
+
+    var xUp = evt.changedTouches[0].clientX;                                    
+    var yUp = evt.changedTouches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    // horizontal swipe should be more significant than verticle
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+      if ( xDiff > 100 ) {
+        /* left swipe */ 
+        clickButton('#next-pic-btn');
+      } 
+      else if ( xDiff < -100 ){
+        /* right swipe */
+        clickButton('#prev-pic-btn');
+      }                       
+    } 
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+  };                                               
+
   // detect keydown events in order to allow a user to use 
   // left arrow (or a) to go to the last picture and
   // right arrow (or d) to go to the next picture
@@ -9,24 +47,29 @@ $(function(){
       case 39:
       // d
       case 68:
-      clickButton('#next-pic-btn')
+      clickButton('#next-pic-btn');
       break;
       // left arrow
       case 37:
       // a
       case 65:
-      clickButton('#prev-pic-btn')
+      clickButton('#prev-pic-btn');
       break;
       default:
       break;
     }
+  };
+
+  function clickButton(name) {
+    var button = $(name)[0];
+    if(button) {
+      button.click();
+    }
   }
+
 });
 
-function clickButton(name) {
-  var button = $(name)[0];
-  if(button) {
-    button.click();
-  }
-}
+
+
+
 
